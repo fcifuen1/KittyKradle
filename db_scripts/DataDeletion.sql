170121f6bc230b9ff5d36a32a5649ddd379f891b
Arthur <huarthu1@umbc.edu>
2018-05-08 22:22:16 -0400

    Added database script. Please read README.

---
+++ db_scripts/DataDeletion.sql
@@ -0,0 +1,5 @@
+DELETE FROM User_Likes_Cat;
+DELETE FROM Cat_Detail;
+DELETE FROM Cat_info;
+DELETE FROM User_Address;
+DELETE FROM User_Info;
---
+++ db_scripts/TableDeletion.sql
@@ -0,0 +1,5 @@
+DROP TABLE User_Likes_Cat;
+DROP TABLE Cat_Detail;
+DROP TABLE Cat_Info;
+DROP TABLE User_Address;
+DROP TABLE User_Info;
