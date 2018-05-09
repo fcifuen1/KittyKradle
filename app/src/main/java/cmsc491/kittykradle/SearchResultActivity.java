d28c5371649f2c41cd36f50902271fdc81a4543f
ChenKuo <ckuo1@umbc.edu>
2018-05-09 00:50:21 -0400

    create activity for search result

---
+++ app/src/main/java/cmsc491/kittykradle/SearchResultActivity.java
@@ -0,0 +1,23 @@
+package cmsc491.kittykradle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+
+    private void showResultGridView(){
+
+    }
+
+    private void showResultListView(){
+
+    }
+
+    private void 
+}
