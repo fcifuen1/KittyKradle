46a102fad60011ffb5dbe22cfec82a12e2f668e8
James <james.a.lambert.94@gmail.com>
2018-05-09 19:01:15 -0400

    Welcome Screen Added welcome splash screen.

--- app/src/main/java/cmsc491/kittykradle/WelcomeScreen.java
+++ app/src/main/java/cmsc491/kittykradle/WelcomeScreen.java
@@ -1,15 +1,24 @@
 package cmsc491.kittykradle;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity
 {
-    // https://medium.com/@ssaurel/create-a-splash-screen-on-android-the-right-way-93d6fb444857
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
