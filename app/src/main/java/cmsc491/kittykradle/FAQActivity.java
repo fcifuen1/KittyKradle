22d86dfbfee9824a10b31d0a8a1028e17dec4243
James <james.a.lambert.94@gmail.com>
2018-05-12 13:41:34 -0400

    FAQ Activity Finished building the FAQ activity, including the rest of the questions and the contact us button, which points to the new contact activity page, which is not yet done.

--- app/src/main/java/cmsc491/kittykradle/FAQActivity.java
+++ app/src/main/java/cmsc491/kittykradle/FAQActivity.java
@@ -1,5 +1,6 @@
 package cmsc491.kittykradle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,8 @@ public class FAQActivity extends SidebarActivity {
     boolean searchTextDisplayed = false;
     boolean adoptTextDisplayed = false;
     boolean approvalTextDisplayed = false;
+    boolean adoptabilityTextDisplayed = false;
+    boolean stateTextDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +70,46 @@ public class FAQActivity extends SidebarActivity {
                 }
             }
         });
+
+        final TextView adoptabilityText = findViewById(R.id.adoptabilityText);
+        ImageButton adoptabilityButton = findViewById(R.id.adoptabilityButton);
+        adoptabilityButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                adoptabilityTextDisplayed = !adoptabilityTextDisplayed;
+                if(adoptabilityTextDisplayed) {
+                    adoptabilityText.setText("It is possible that a cat that is still in the app is going through the adoption process with another user. In that case, it is possible the cat will not be removed until the adoption process is complete.");
+                }
+                else {
+                    adoptabilityText.setText("");
+                }
+            }
+        });
+
+        final TextView stateText = findViewById(R.id.stateText);
+        ImageButton stateButton = findViewById(R.id.stateButton);
+        stateButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                stateTextDisplayed = !stateTextDisplayed;
+                if(stateTextDisplayed) {
+                    stateText.setText("Yes, you can adopt a cat from out of state.");
+                }
+                else {
+                    stateText.setText("");
+                }
+            }
+        });
+
+        ImageButton contactButton = findViewById(R.id.contactButton);
+        contactButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
