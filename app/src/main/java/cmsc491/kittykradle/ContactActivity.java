e9cea8c1bfb9188bf97941d0aaf65936f3273869
James <james.a.lambert.94@gmail.com>
2018-05-12 17:27:11 -0400

    Contact Activity Finished building the contact activity page, including buttons that start an intent for the phone's browser to open "our website" and a button that returns to the faq page.

--- app/src/main/java/cmsc491/kittykradle/ContactActivity.java
+++ app/src/main/java/cmsc491/kittykradle/ContactActivity.java
@@ -1,11 +1,15 @@
 package cmsc491.kittykradle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -16,14 +20,26 @@ public class ContactActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button linkButton = findViewById(R.id.linkButton);
+        linkButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://kittykradleadoption.com"));
+                startActivity(intent);
             }
         });
+
+        ImageButton faqButton = findViewById(R.id.faqButton);
+        faqButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), FAQActivity.class);
+                startActivity(intent);
+                finish(); //this page doesn't need to remain on the activity stack
+            }
+        });
+
     }
 
 }
