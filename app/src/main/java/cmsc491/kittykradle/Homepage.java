850bd92c558568f7641c1912392aa3df4fa9754d
ChenKuo <ckuo1@umbc.edu>
2018-05-14 10:35:53 -0400

    homepage

--- app/src/main/java/cmsc491/kittykradle/Homepage.java
+++ app/src/main/java/cmsc491/kittykradle/Homepage.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends SidebarActivity {
 
     Button toSearch, toFaq;
 
