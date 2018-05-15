240bf6bc0d5b754d1d1a47ac9a6cb5806e7a8cdc
ChenKuo <ckuo1@umbc.edu>
2018-05-15 15:07:58 -0400

    database insertion

--- db_scripts/DataInsertion.sql
+++ db_scripts/DataInsertion.sql
@@ -1,43 +1,43 @@
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) VALUES ("Mr. Pickles", "21250", "0", "He does NOT like pickles.", "http://i572.photobucket.com/albums/ss166/cheng_cki/Cat001.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Tabby", "M", "50123", "5");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Tabby", "M", "50123", "M");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) VALUES ("Bubbers", "21251", "0", "She likes bubbles.", "http://i572.photobucket.com/albums/ss166/cheng_cki/Cat001.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Ginger", "F", "1", "2");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Ginger", "F", "1", "M");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) VALUES ("Meower", "20534", "0", "He'll meow the heck out of you.", "http://i298.photobucket.com/albums/mm261/ilikecheese4lyf/l_d16d6fe9d20451d4a89203e99b862045.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "White", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "White", "M", "3", "L");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) VALUES ("Bubbers", "21251", "0", "He is not the other Bubbers. Eventhough they look the same.", "http://i572.photobucket.com/albums/ss166/cheng_cki/Cat001.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "M");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Angel", "21221", "0", "Is this not an angel", "http://i1126.photobucket.com/albums/l606/kuochine/angel_zpskxewvgnw.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "S");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Wombat", "21221", "0", "Wombat is a wom..bat...", "http://i1126.photobucket.com/albums/l606/kuochine/wombat_zpszfcg8lm8.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "L");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Butter", "21221", "0", "Just like butter", "http://i1126.photobucket.com/albums/l606/kuochine/butters_zpsr0bl0mev.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "M");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Joey", "21221", "0", "Pride and joy", "http://i1126.photobucket.com/albums/l606/kuochine/joey_zpsniyjhxov.jpeg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "M");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Nando", "21221", "0", "Nanananando", "http://i1126.photobucket.com/albums/l606/kuochine/nando_zpsqtire7hv.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "S");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Cucumber", "21221", "0", "See her reaction when she see a cucumber", "http://i1126.photobucket.com/albums/l606/kuochine/cucumber_zpsnepyeosq.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "S");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Jasmine", "21221", "0", "It's just Jasmine", "http://i1126.photobucket.com/albums/l606/kuochine/jasmine_zpslwaba5va.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "M", "3", "L");
 
 INSERT INTO `Cat_Info` (`name`, `zipcode`, `likes`, `bio`, `picture_url`) 
 VALUES ("Paisley", "21221", "0", "Paisley is a good name for this cat", "http://i1126.photobucket.com/albums/l606/kuochine/paisley_zpseh1v9saj.jpg");
-INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "6");
+INSERT INTO `Cat_Detail` VALUES(LAST_INSERT_ID(), "Black", "F", "3", "M");
